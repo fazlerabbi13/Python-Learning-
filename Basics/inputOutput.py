a45dac5ccb5fb7f9f62a3f6c926328800dc1da87
fazlerabbi13 <mohammad0132794@gmail.com>
2025-11-03 17:57:02 +0600

    input an integer.

--- Basics/inputOutput.py
+++ Basics/inputOutput.py
@@ -2,7 +2,12 @@
 # print("hello", name)
 
 # multiple input
-x , y = input("enter two value: ").split()
-print("number of apple: ",x)
-print("number of banana: ",y)
+# x , y = input("enter two value: ").split()
+# print("number of apple: ",x)
+# print("number of banana: ",y)
+
+# input integer
+
+n = int(input("how old are you: "))
+print(n)
       
