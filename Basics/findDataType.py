b833e000ac016195720d258a6fa13a39ea9a0f80
fazlerabbi13 <mohammad0132794@gmail.com>
2025-11-03 18:11:17 +0600

    determine the type of an object.

---
+++ Basics/findDataType.py
@@ -0,0 +1,14 @@
+a = "Hello world"
+b = 10
+c = 10.56
+d = ("hello","world")
+e = ["hello","world"]
+f = {"hello":1,"world":2}
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+
