d894a0a6e208e950d7746339a089a5662ea564fa
fazlerabbi13 <mohammad0132794@gmail.com>
2025-11-04 16:10:35 +0600

    Ternary Operator in Nested If else.

--- TernaryOperator.py
+++ TernaryOperator.py
@@ -1,3 +1,9 @@
-user  = int(input("Enter number: "))
-result = "Even" if user % 2 == 0 else "Odd"
+# user  = int(input("Enter number: "))
+# result = "Even" if user % 2 == 0 else "Odd"
+# print(result)
+
+
+# Ternary Operator in Nested If else
+user = int(input("Enter number: "))
+result = "Possitive" if user > 0 else "Negative" if user < 0 else "Zero"
 print(result)
