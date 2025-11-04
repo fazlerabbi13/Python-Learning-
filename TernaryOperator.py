0bdca8f59fed71419efa1fe943ee8c95fd9f4c3e
fazlerabbi13 <mohammad0132794@gmail.com>
2025-11-04 16:27:36 +0600

    Ternary Operator using Python Lambda.

--- TernaryOperator.py
+++ TernaryOperator.py
@@ -14,7 +14,13 @@
 # print(result)
 
 # Ternary Operator using Dictionary
+# userIn1 =int(input("Enter Number 1: ")) 
+# userIn2 =int(input("Enter Number 2: "))
+# max = {True: userIn1, False: userIn2}[userIn1 > userIn2]
+# print(max)
+
+# Ternary Operator using Python Lambda
 userIn1 =int(input("Enter Number 1: ")) 
 userIn2 =int(input("Enter Number 2: "))
-max = {True: userIn1, False: userIn2}[userIn1 > userIn2]
+max = (lambda userIn1,userIn2: userIn1 if userIn1 > userIn2 else userIn2)(userIn1,userIn2)
 print(max)
