31ad60b01294bda08f7257c9adb88862c2600a96
fazlerabbi13 <mohammad0132794@gmail.com>
2025-11-05 20:10:15 +0600

    Nested if..else means an if-else statement inside another if statement. We can use nested if statements to check conditions within conditions.

--- Basics/conditions.py
+++ Basics/conditions.py
@@ -6,13 +6,30 @@
 
 
 # elif Statement
-age = int(input("Enter age: "))
+# age = int(input("Enter age: "))
 
-if age <= 15:
-    print("Child")
-elif age <= 18:
-    print("Teenager")
-elif age <= 30:
-    print("Young Adult")
+# if age <= 15:
+#     print("Child")
+# elif age <= 18:
+#     print("Teenager")
+# elif age <= 30:
+#     print("Young Adult")
+# else:
+#     print("Adult")
+
+
+# Nested if..else Conditional Statement
+
+age = int(input("Enter age: "))
+# age = 15
+is_eligible = True
+
+if age >= 20:
+    if is_eligible:
+        print("eligible for vote")
+    else:
+        print("not eligible")
 else:
-    print("Adult")
+    print("Child")
+
+  
