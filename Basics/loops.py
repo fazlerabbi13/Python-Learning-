238801fb8b0e503c673f6c079e83963df0d6ab11
fazlerabbi13 <mohammad0132794@gmail.com>
2025-11-06 16:47:45 +0600

    iteration set.

--- Basics/loops.py
+++ Basics/loops.py
@@ -19,6 +19,11 @@
 #     print(s)
 
 # iterating dictionary
-dictionary = dict({'id':47014, 'reg':241245789})
-for i in dictionary:
-    print("%s %d" % (i, dictionary[i]))
+# dictionary = dict({'id':47014, 'reg':241245789})
+# for i in dictionary:
+#     print("%s %d" % (i, dictionary[i]))
+
+# iteration in set
+set1 = {10, 20 ,30}
+for i in set1:
+    print(i)
