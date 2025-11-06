997524bdb0efa598eca3175b51352954f136802d
fazlerabbi13 <mohammad0132794@gmail.com>
2025-11-06 17:11:12 +0600

    break statement is used to exit the loop prematurely when a specified condition is met.

--- Basics/loops.py
+++ Basics/loops.py
@@ -34,21 +34,29 @@
 #     print(li[i])
 
 # while loop
-count = 0
-while(count < 3):
-    count = count + 1
-    print("Hello world")
+# count = 0
+# while(count < 3):
+#     count = count + 1
+#     print("Hello world")
 
 
 # Nested Loops
 
-for i in range(1, 5):
-    for j in range(i):
-        print(i,end=' ')
-    print()
+# for i in range(1, 5):
+#     for j in range(i):
+#         print(i,end=' ')
+#     print()
 
 # Continue Statement
+# for l in "HelloWorld":
+#     if l == 'e' or l == 'r':
+#         continue
+#     print('current letter: ', l)
+
+
+# brak statement
 for l in "HelloWorld":
     if l == 'e' or l == 'r':
-        continue
+        break
+        
     print('current letter: ', l)
