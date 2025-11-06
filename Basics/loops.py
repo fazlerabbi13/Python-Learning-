3ae09bf502053ec3129a2fd05c0c251c8a304644
fazlerabbi13 <mohammad0132794@gmail.com>
2025-11-06 15:28:45 +0600

    iterating tuple.

--- Basics/loops.py
+++ Basics/loops.py
@@ -3,6 +3,12 @@
 # for i in range(0,n):
 #     print(i)
 
-list = ["rabbi", "anayet","tufazzal"]
-for i in list:
+# list iterating
+# list = ["rabbi", "anayet","tufazzal"]
+# for i in list:
+#     print(i)
+
+# tuple iterating
+tuple = ("hello","programming","world")
+for i in tuple:
     print(i)
