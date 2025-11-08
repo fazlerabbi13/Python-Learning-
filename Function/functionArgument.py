da1c3d451c258045b8b00e190d85fb2ddaf15094
fazlerabbi13 <mohammad0132794@gmail.com>
2025-11-08 18:52:30 +0600

    A default argument is a parameter that assumes a default value if a value is not provided in the function call for that argument.

--- Function/functionArgument.py
+++ Function/functionArgument.py
@@ -1,8 +1,15 @@
-def evenOdd(n):
-    if(n % 2 == 0):
-        return "Even"
-    else:
-        return "Odd"
+# def evenOdd(n):
+#     if(n % 2 == 0):
+#         return "Even"
+#     else:
+#         return "Odd"
     
-print(evenOdd(5))
-print(evenOdd(10))
+# print(evenOdd(5))
+# print(evenOdd(10))
+
+# default Argument
+
+def func(x,y=20):
+    print("x = ",x)
+    print("y = ",y)
+func(10)
