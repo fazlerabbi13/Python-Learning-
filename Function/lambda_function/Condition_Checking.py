0446ecc3658bdb05a700f55dcda1cce8570fa9a1
fazlerabbi13 <mohammad0132794@gmail.com>
2025-12-26 20:47:11 +0600

    checking even and odd number using lambda function.

--- Function/lambda_function/Condition_Checking.py
+++ Function/lambda_function/Condition_Checking.py
@@ -1,5 +1,11 @@
-n = lambda x: "positive" if x > 0 else "Negative" if x < 0 else "zero"
+# the lambda function uses nested if-else logic to classify numbers as Positive, Negative or Zero.
+# n = lambda x: "positive" if x > 0 else "Negative" if x < 0 else "zero"
 
-print(n(5))
-print(n(-5))
-print(n(0))
+# print(n(5))
+# print(n(-5))
+# print(n(0))
+
+# This lambda checks divisibility by 2 and returns "Even" or "Odd" accordingly.
+check = lambda x: "even" if x % 2 == 0 else "odd"
+print(check(4))
+print(check(3))
