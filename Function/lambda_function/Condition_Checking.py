0bcff77f79c7a5f29822cae974d7d518a3831335
fazlerabbi13 <mohammad0132794@gmail.com>
2025-12-27 23:18:46 +0600

    calculates both sum and product of two numbers and returns them as a tuple using lambda function.

--- Function/lambda_function/Condition_Checking.py
+++ Function/lambda_function/Condition_Checking.py
@@ -6,6 +6,11 @@
 # print(n(0))
 
 # This lambda checks divisibility by 2 and returns "Even" or "Odd" accordingly.
-check = lambda x: "even" if x % 2 == 0 else "odd"
-print(check(4))
-print(check(3))
+# check = lambda x: "even" if x % 2 == 0 else "odd"
+# print(check(4))
+# print(check(3))
+
+# The lambda calculates both sum and product of two numbers and returns them as a tuple.
+calculate = lambda x,y:(x + y,x * y)
+res = calculate(3, 4)
+print(res)
