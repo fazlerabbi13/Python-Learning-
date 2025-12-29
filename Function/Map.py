9cda6edfdc555312c202a1d0c6d76ce19fa2e03a
fazlerabbi13 <mohammad0132794@gmail.com>
2025-12-29 21:47:53 +0600

    map() function returns a map object, which is an iterator. In many cases, we will need to convert this iterator to a list to work with the results directly.make cube of every number in list.

--- Function/Map.py
+++ Function/Map.py
@@ -1,3 +1,10 @@
 nums = [1, 2, 3, 4, 5]
 result = map(int, nums)
 print(list(result))
+
+# Converting map object to a list
+def cube(val):
+    return val * val *val
+
+result = list(map(cube, nums))
+print(result)
