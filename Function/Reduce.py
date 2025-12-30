d6a7ff5699f74916c8a4c8f8813a546e76309c96
fazlerabbi13 <mohammad0132794@gmail.com>
2025-12-30 22:54:16 +0600

    demonstrates how a lambda function can be used with reduce() to calculate factorial of a number by multiplying all elements of a list.

--- Function/Reduce.py
+++ Function/Reduce.py
@@ -1,7 +1,14 @@
-from functools import reduce
-def add(x, y):
-    return x + y
+#  adding two numbers at a time.
+# from functools import reduce
+# def add(x, y):
+#     return x + y
 
+# nums = [1, 2, 3, 4, 5]
+# result = reduce(add, nums)
+# print(result)
+
+#  calculate factorial of a number by multiplying all elements of a list.
+from functools import reduce
 nums = [1, 2, 3, 4, 5]
-result = reduce(add, nums)
+result = reduce(lambda x, y: x * y, nums)
 print(result)
